8ee88d053c762db54d6538b261fe6f7c76ee39d3
EvgeniySuharev <169095875+EvgeniySuharev@users.noreply.github.com>
2024-05-15 23:05:11 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+# Работа со списками:
+my_list = ['apple', 'banana', 'kiwi', 'lemon', 'mango', 'pineapple']
+print('My list:', my_list)
+print('First element:', my_list[0])
+print('Last element', my_list[-1])
+print('Sublist:', my_list[2:5])
+my_list[2] = 'orange'
+print('My new list:', my_list)
+# Работа со словарями:
+my_dict = {'black': 'черный', 'white': 'белый', 'green': 'зеленый'}
+print('My dictionary:', my_dict)
+print('Translation: Black -', my_dict['black'])
+my_dict['black'] = 'розовый'
+my_dict['grey'] = 'серый'
+print('My new dictionary:', my_dict)
